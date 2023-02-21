a7a986a3fd75ec2b8c7e4c1888a559c3f806ad8f
Michell Pacheco Tacay <90942815+silverblade34@users.noreply.github.com>
2023-02-21 12:00:10 -0500

    Listo despliegue

--- routes/credential_route.py
+++ routes/credential_route.py
@@ -2,8 +2,8 @@ from flask import Flask, request, render_template, redirect, url_for, session
 from flask_cors import CORS
 from src.credential.infrastructure.controller import CredentialController 
 
-#from app import app
-from __main__ import app
+from app import app
+#from __main__ import app
 app.secret_key = "hhyy526//--"
 CORS(app)
 
