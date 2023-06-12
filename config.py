b418bf7cf3c94a3c90b88ae1c008033f6f40df61
silverblade34 <mpacheco.tacay@gmail.com>
2023-06-12 17:56:55 -0500

    adaptado uso libre

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import os
 
-#API_SERVER = "http://192.168.1.44:3000" 
-API_SERVER = "http://161.35.104.161:3000"
+API_SERVER = "url_api"
 UPLOAD_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static', 'img', 'logos')
 
